33c70b2f22e0b819b68be24e834d699b38bada3d
cmshalom <cmshalom@cmshalom-think>
2018-12-27 13:26:20 +0200

    Received from Oylum

---
+++ ChordalGraphGeneration/generate.sh
@@ -0,0 +1,160 @@
+./Debug/ChordalGraphGeneration 1000 0.01 1 0.011
+./Debug/ChordalGraphGeneration 1000 0.01 2 0.011
+./Debug/ChordalGraphGeneration 1000 0.01 3 0.011
+./Debug/ChordalGraphGeneration 1000 0.01 4 0.011
+./Debug/ChordalGraphGeneration 1000 0.01 5 0.011
+./Debug/ChordalGraphGeneration 1000 0.01 6 0.011
+./Debug/ChordalGraphGeneration 1000 0.01 7 0.011
+./Debug/ChordalGraphGeneration 1000 0.01 8 0.011
+./Debug/ChordalGraphGeneration 1000 0.01 9 0.011
+./Debug/ChordalGraphGeneration 1000 0.01 10 0.011
+./Debug/ChordalGraphGeneration 1000 0.1 1 0.16
+./Debug/ChordalGraphGeneration 1000 0.1 2 0.16
+./Debug/ChordalGraphGeneration 1000 0.1 3 0.16
+./Debug/ChordalGraphGeneration 1000 0.1 4 0.16
+./Debug/ChordalGraphGeneration 1000 0.1 5 0.16
+./Debug/ChordalGraphGeneration 1000 0.1 6 0.16
+./Debug/ChordalGraphGeneration 1000 0.1 7 0.16
+./Debug/ChordalGraphGeneration 1000 0.1 8 0.16
+./Debug/ChordalGraphGeneration 1000 0.1 9 0.16
+./Debug/ChordalGraphGeneration 1000 0.1 10 0.16
+./Debug/ChordalGraphGeneration 1000 0.5 1
+./Debug/ChordalGraphGeneration 1000 0.5 2
+./Debug/ChordalGraphGeneration 1000 0.5 3
+./Debug/ChordalGraphGeneration 1000 0.5 4
+./Debug/ChordalGraphGeneration 1000 0.5 5
+./Debug/ChordalGraphGeneration 1000 0.5 6
+./Debug/ChordalGraphGeneration 1000 0.5 7
+./Debug/ChordalGraphGeneration 1000 0.5 8
+./Debug/ChordalGraphGeneration 1000 0.5 9
+./Debug/ChordalGraphGeneration 1000 0.5 10
+./Debug/ChordalGraphGeneration 1000 0.8 1
+./Debug/ChordalGraphGeneration 1000 0.8 2
+./Debug/ChordalGraphGeneration 1000 0.8 3
+./Debug/ChordalGraphGeneration 1000 0.8 4
+./Debug/ChordalGraphGeneration 1000 0.8 5
+./Debug/ChordalGraphGeneration 1000 0.8 6
+./Debug/ChordalGraphGeneration 1000 0.8 7
+./Debug/ChordalGraphGeneration 1000 0.8 8
+./Debug/ChordalGraphGeneration 1000 0.8 9
+./Debug/ChordalGraphGeneration 1000 0.8 10
+./Debug/ChordalGraphGeneration 2500 0.01 1 0.011
+./Debug/ChordalGraphGeneration 2500 0.01 2 0.011
+./Debug/ChordalGraphGeneration 2500 0.01 3 0.011
+./Debug/ChordalGraphGeneration 2500 0.01 4 0.011
+./Debug/ChordalGraphGeneration 2500 0.01 5 0.011
+./Debug/ChordalGraphGeneration 2500 0.01 6 0.011
+./Debug/ChordalGraphGeneration 2500 0.01 7 0.011
+./Debug/ChordalGraphGeneration 2500 0.01 8 0.011
+./Debug/ChordalGraphGeneration 2500 0.01 9 0.011
+./Debug/ChordalGraphGeneration 2500 0.01 10 0.011
+./Debug/ChordalGraphGeneration 2500 0.1 1 0.16
+./Debug/ChordalGraphGeneration 2500 0.1 2 0.16
+./Debug/ChordalGraphGeneration 2500 0.1 3 0.16
+./Debug/ChordalGraphGeneration 2500 0.1 4 0.16
+./Debug/ChordalGraphGeneration 2500 0.1 5 0.16
+./Debug/ChordalGraphGeneration 2500 0.1 6 0.16
+./Debug/ChordalGraphGeneration 2500 0.1 7 0.16
+./Debug/ChordalGraphGeneration 2500 0.1 8 0.16
+./Debug/ChordalGraphGeneration 2500 0.1 9 0.16
+./Debug/ChordalGraphGeneration 2500 0.1 10 0.16
+./Debug/ChordalGraphGeneration 2500 0.5 1
+./Debug/ChordalGraphGeneration 2500 0.5 2
+./Debug/ChordalGraphGeneration 2500 0.5 3
+./Debug/ChordalGraphGeneration 2500 0.5 4
+./Debug/ChordalGraphGeneration 2500 0.5 5
+./Debug/ChordalGraphGeneration 2500 0.5 6
+./Debug/ChordalGraphGeneration 2500 0.5 7
+./Debug/ChordalGraphGeneration 2500 0.5 8
+./Debug/ChordalGraphGeneration 2500 0.5 9
+./Debug/ChordalGraphGeneration 2500 0.5 10
+./Debug/ChordalGraphGeneration 2500 0.8 1
+./Debug/ChordalGraphGeneration 2500 0.8 2
+./Debug/ChordalGraphGeneration 2500 0.8 3
+./Debug/ChordalGraphGeneration 2500 0.8 4
+./Debug/ChordalGraphGeneration 2500 0.8 5
+./Debug/ChordalGraphGeneration 2500 0.8 6
+./Debug/ChordalGraphGeneration 2500 0.8 7
+./Debug/ChordalGraphGeneration 2500 0.8 8
+./Debug/ChordalGraphGeneration 2500 0.8 9
+./Debug/ChordalGraphGeneration 2500 0.8 10
+./Debug/ChordalGraphGeneration 5000 0.01 1 0.011
+./Debug/ChordalGraphGeneration 5000 0.01 2 0.011
+./Debug/ChordalGraphGeneration 5000 0.01 3 0.011
+./Debug/ChordalGraphGeneration 5000 0.01 4 0.011
+./Debug/ChordalGraphGeneration 5000 0.01 5 0.011
+./Debug/ChordalGraphGeneration 5000 0.01 6 0.011
+./Debug/ChordalGraphGeneration 5000 0.01 7 0.011
+./Debug/ChordalGraphGeneration 5000 0.01 8 0.011
+./Debug/ChordalGraphGeneration 5000 0.01 9 0.011
+./Debug/ChordalGraphGeneration 5000 0.01 10 0.011
+./Debug/ChordalGraphGeneration 5000 0.1 1 0.16
+./Debug/ChordalGraphGeneration 5000 0.1 2 0.16
+./Debug/ChordalGraphGeneration 5000 0.1 3 0.16
+./Debug/ChordalGraphGeneration 5000 0.1 4 0.16
+./Debug/ChordalGraphGeneration 5000 0.1 5 0.16
+./Debug/ChordalGraphGeneration 5000 0.1 6 0.16
+./Debug/ChordalGraphGeneration 5000 0.1 7 0.16
+./Debug/ChordalGraphGeneration 5000 0.1 8 0.16
+./Debug/ChordalGraphGeneration 5000 0.1 9 0.16
+./Debug/ChordalGraphGeneration 5000 0.1 10 0.16
+./Debug/ChordalGraphGeneration 5000 0.5 1
+./Debug/ChordalGraphGeneration 5000 0.5 2
+./Debug/ChordalGraphGeneration 5000 0.5 3
+./Debug/ChordalGraphGeneration 5000 0.5 4
+./Debug/ChordalGraphGeneration 5000 0.5 5
+./Debug/ChordalGraphGeneration 5000 0.5 6
+./Debug/ChordalGraphGeneration 5000 0.5 7
+./Debug/ChordalGraphGeneration 5000 0.5 8
+./Debug/ChordalGraphGeneration 5000 0.5 9
+./Debug/ChordalGraphGeneration 5000 0.5 10
+./Debug/ChordalGraphGeneration 5000 0.8 1
+./Debug/ChordalGraphGeneration 5000 0.8 2
+./Debug/ChordalGraphGeneration 5000 0.8 3
+./Debug/ChordalGraphGeneration 5000 0.8 4
+./Debug/ChordalGraphGeneration 5000 0.8 5
+./Debug/ChordalGraphGeneration 5000 0.8 6
+./Debug/ChordalGraphGeneration 5000 0.8 7
+./Debug/ChordalGraphGeneration 5000 0.8 8
+./Debug/ChordalGraphGeneration 5000 0.8 9
+./Debug/ChordalGraphGeneration 5000 0.8 10
+./Debug/ChordalGraphGeneration 10000 0.01 1 0.011
+./Debug/ChordalGraphGeneration 10000 0.01 2 0.011
+./Debug/ChordalGraphGeneration 10000 0.01 3 0.011
+./Debug/ChordalGraphGeneration 10000 0.01 4 0.011
+./Debug/ChordalGraphGeneration 10000 0.01 5 0.011
+./Debug/ChordalGraphGeneration 10000 0.01 6 0.011
+./Debug/ChordalGraphGeneration 10000 0.01 7 0.011
+./Debug/ChordalGraphGeneration 10000 0.01 8 0.011
+./Debug/ChordalGraphGeneration 10000 0.01 9 0.011
+./Debug/ChordalGraphGeneration 10000 0.01 10 0.011
+./Debug/ChordalGraphGeneration 10000 0.1 1 0.16
+./Debug/ChordalGraphGeneration 10000 0.1 2 0.16
+./Debug/ChordalGraphGeneration 10000 0.1 3 0.16
+./Debug/ChordalGraphGeneration 10000 0.1 4 0.16
+./Debug/ChordalGraphGeneration 10000 0.1 5 0.16
+./Debug/ChordalGraphGeneration 10000 0.1 6 0.16
+./Debug/ChordalGraphGeneration 10000 0.1 7 0.16
+./Debug/ChordalGraphGeneration 10000 0.1 8 0.16
+./Debug/ChordalGraphGeneration 10000 0.1 9 0.16
+./Debug/ChordalGraphGeneration 10000 0.1 10 0.16
+./Debug/ChordalGraphGeneration 10000 0.5 1
+./Debug/ChordalGraphGeneration 10000 0.5 2
+./Debug/ChordalGraphGeneration 10000 0.5 3
+./Debug/ChordalGraphGeneration 10000 0.5 4
+./Debug/ChordalGraphGeneration 10000 0.5 5
+./Debug/ChordalGraphGeneration 10000 0.5 6
+./Debug/ChordalGraphGeneration 10000 0.5 7
+./Debug/ChordalGraphGeneration 10000 0.5 8
+./Debug/ChordalGraphGeneration 10000 0.5 9
+./Debug/ChordalGraphGeneration 10000 0.5 10
+./Debug/ChordalGraphGeneration 10000 0.8 1
+./Debug/ChordalGraphGeneration 10000 0.8 2
+./Debug/ChordalGraphGeneration 10000 0.8 3
+./Debug/ChordalGraphGeneration 10000 0.8 4
+./Debug/ChordalGraphGeneration 10000 0.8 5
+./Debug/ChordalGraphGeneration 10000 0.8 6
+./Debug/ChordalGraphGeneration 10000 0.8 7
+./Debug/ChordalGraphGeneration 10000 0.8 8
+./Debug/ChordalGraphGeneration 10000 0.8 9
+./Debug/ChordalGraphGeneration 10000 0.8 10
